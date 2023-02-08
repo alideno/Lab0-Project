d4e7ede88bbf597c5eb4bdd34cc14fe3908a3039
Ali Deniz <alidenizsozer@gmail.com>
2023-02-08 18:47:12 +0300

    Fixed an error in main file

--- Lab0.java
+++ Lab0.java
@@ -6,7 +6,6 @@
  * @edits Deniz Şahin
  */
 
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -32,66 +31,71 @@ class Lab0
             // System.out.println(randomNumber); // Control purposes
             numbers[i] = randomNumber;
         }
-
-        String choice;
-
-        do
-        {
-
-            System.out.println("\nPlease choose one of the options below:\n1. Find the minimum and the maximum of the array\n" 
-                           + "2.Find the average of the array. Display how each element of the array differs from the average\n"
-                           + "3.Find the sum of elements with odd- and even-numbered indexes\n"
-                           + "4.Exit\n");
-
-            System.out.print("Choose an option: ");
-            input = new Scanner(System.in);
-            choice = input.nextLine();
-
-            if ( choice.equals("1"))
+        System.out.println(average.ArrayString(numbers));
+        return numbers;
+    }
+        public static void main(String[] args) {
+            String choice;
+            int[] numbers = randomArray();
+            do
             {
-                System.out.println("\n1.Find the minimum of the array\n2.Find the maximum of the array");
-                input = new Scanner(System.in);
+    
+                System.out.println("\nPlease choose one of the options below:\n1. Find the minimum and the maximum of the array\n" 
+                               + "2.Find the average of the array. Display how each element of the array differs from the average\n"
+                               + "3.Find the sum of elements with odd- and even-numbered indexes\n"
+                               + "4.Exit\n");
+    
+                System.out.print("Choose an option: ");
+                Scanner input = new Scanner(System.in);
                 choice = input.nextLine();
-
-                if ( choice.equals("1" ))
+    
+                if ( choice.equals("1"))
                 {
-                    System.out.println("The minimum of the array is " + third.minValue(numbers));
+                    System.out.println("\n1.Find the minimum of the array\n2.Find the maximum of the array");
+                    input = new Scanner(System.in);
+                    choice = input.nextLine();
+    
+                    if ( choice.equals("1" ))
+                    {
+                        System.out.println("The minimum of the array is " + third.minValue(numbers));
+                    }
+                    else if ( choice.equals("2"))
+                    {
+                        System.out.println("The maximum of the array is " + third.maxValue(numbers));
+                    }
+    
+    
                 }
+    
                 else if ( choice.equals("2"))
                 {
-                    System.out.println("The maximum of the array is " + third.maxValue(numbers));
+                    System.out.println("\n1.Find the average of the array\n2.Display how each element of the array differs from the average");
+                    input = new Scanner(System.in);
+                    choice = input.nextLine();
+                    if (choice.equals("1"))
+                    {
+                         System.out.println("Average of the array is " + average.getAverage(numbers));
+                    }
+                    else if (choice.equals("2"))
+                    {
+                        System.out.println("Difference of each element relative to the average: " + average.ArrayString(average.getDifferance(numbers)));
+                    }
                 }
-
-
-            }
-
-            else if ( choice.equals("2"))
-            {
-                System.out.println("\n1.Find the average of the array\n2.Display how each element of the array differs from the average");
-                input = new Scanner(System.in);
-                choice = input.nextLine();
-                if (choice.equals("1"))
+                else if ( choice.equals("3"))
                 {
-                     System.out.println("Average of the array is " + average.getAverage(numbers));
-                }
-                else if (choice.equals("2"))
-                {
-                    System.out.println("Difference of each element relative to the average: " + average.ArrayString(average.getDifferance(numbers)));
+                        System.out.println("The sum of elements with even-numbered indexes are " + question5.groupAddNumbers(numbers)[0] 
+                                        + ". The sum of elements with odd-numbered indexes are " + question5.groupAddNumbers(numbers)[1]);
                 }
             }
-            else if ( choice.equals("3"))
-            {
-                    System.out.println("The sum of elements with even-numbered indexes are " + question5.groupAddNumbers(numbers)[0] 
-                                    + ". The sum of elements with odd-numbered indexes are " + question5.groupAddNumbers(numbers)[1]);
-            }
+            
+            while ( !(choice.equals("4")));   
+    
+            System.out.println("\nThe end of the program");
+    
+    
+    
+            
+        
         }
-        
-        while ( !(choice.equals("4")));   
 
-        System.out.println("\nThe end of the program");
-
-
-
-        
-    }
 }
